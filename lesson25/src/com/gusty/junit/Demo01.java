77fdeed9ec96f5ac1a4d1ed01143b91f19c0ace8
Gusty <a0985209465@gmail.com>
2024-10-03 11:25:36 +0800

    Update Demo01.java

--- lesson25/src/com/gusty/junit/Demo01.java
+++ lesson25/src/com/gusty/junit/Demo01.java
@@ -1,13 +1,50 @@
 package com.gusty.junit;
 
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 public class Demo01 {
 
-    @Test
-    public void Test123() {
-        System.out.println("123456");
+  @Test
+  public void testMain() {
+//        Assertions.assertEquals(rob(new int[]{2,3,2}),3);
+//        System.out.println("Hello World");
+    System.out.println();
+    countSegments("    foo    bar");
+  }
+
+  public int countSegments(String s) {
+    s = s.trim();
+    if (s.isEmpty()) {
+      return 0;
     }
+    while (s.contains("  ")) {
+      s = s.replaceAll("  ", " ");
+    }
+    System.out.println(s);
+    String[] newStrAry = s.split(" ");
+    System.out.println(Arrays.toString(newStrAry));
+    System.out.println(newStrAry.length);
+    return newStrAry.length;
+//    return 0 ;
+  }
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
 }
